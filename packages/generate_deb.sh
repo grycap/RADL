559d2f033f21db96be656f331dfe548dc6365abd
micafer <micafer1@upv.es>
2016-06-30 13:48:54 +0200

    cp deb and rpm file to dist_pkg dir

--- packages/generate_deb.sh
+++ packages/generate_deb.sh
@@ -3,4 +3,5 @@
 apt update
 apt install -y dh-make python-stdeb fakeroot python-all python-setuptools
 python setup.py --command-packages=stdeb.command sdist_dsc --depends "python-ply" bdist_deb
-cp deb_dist/*.deb dist/
+mkdir dist_pkg
+cp deb_dist/*.deb dist_pkg
--- packages/generate_rpm.sh
+++ packages/generate_rpm.sh
@@ -3,3 +3,5 @@
 yum -y install rpm-build python-setuptools
 echo "%_unpackaged_files_terminate_build 0" > ~/.rpmmacros
 python setup.py bdist_rpm --requires="python-ply"
+mkdir dist_pkg
+cp dist/*.noarch.rpm dist_pkg
