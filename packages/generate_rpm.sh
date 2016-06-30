0d13d12646c665ab8eb32a041f08d9516a906ec0
micafer <micafer1@upv.es>
2016-06-30 12:17:59 +0200

    Quit git clone from0 scripts to generate rpm and deb packages

--- packages/generate_rpm.sh
+++ packages/generate_rpm.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-yum -y install rpm-build git python-setuptools
+yum -y install rpm-build python-setuptools
 mkdir /tmp/rpms
 echo "%_unpackaged_files_terminate_build 0" > ~/.rpmmacros
 
-git clone https://github.com/grycap/radl
-cd radl
 python setup.py bdist_rpm --requires="python-ply"
 cp dist/*.noarch.rpm /tmp/rpms
-
