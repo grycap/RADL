3b3b869f70dc9167bb04b3f2afd9751b9f64b404
micafer <micafer1@upv.es>
2016-06-30 13:20:43 +0200

    Quit cp from scripts to generate rpm and deb packages

--- packages/generate_deb.sh
+++ packages/generate_deb.sh
@@ -2,7 +2,4 @@
 
 apt update
 apt install -y dh-make python-stdeb fakeroot python-all python-setuptools
-mkdir /tmp/debs
-
 python setup.py --command-packages=stdeb.command sdist_dsc --depends "python-ply" bdist_deb
-cp deb_dist/*.deb /tmp/debs
--- packages/generate_rpm.sh
+++ packages/generate_rpm.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 yum -y install rpm-build python-setuptools
-mkdir /tmp/rpms
 echo "%_unpackaged_files_terminate_build 0" > ~/.rpmmacros
-
 python setup.py bdist_rpm --requires="python-ply"
-cp dist/*.noarch.rpm /tmp/rpms
