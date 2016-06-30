95c0cebecfa9c244481a1ec39cd9498b1878c547
micafer <micafer1@upv.es>
2016-06-30 15:22:03 +0200

    Remove unnecesary packages

--- packages/generate_deb.sh
+++ packages/generate_deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install -y dh-make python-stdeb fakeroot python-all python-setuptools
+apt install -y dh-make python-stdeb
 python setup.py --command-packages=stdeb.command sdist_dsc --depends "python-ply" bdist_deb
 mkdir dist_pkg
 cp deb_dist/*.deb dist_pkg
