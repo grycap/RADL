7c46e073ba04fd7b39b867f84af583eabcb5cd78
micafer <micafer1@upv.es>
2016-07-01 10:59:34 +0200

    Set release as parameter

--- packages/generate_rpm.sh
+++ packages/generate_rpm.sh
@@ -2,6 +2,6 @@
 
 yum -y install rpm-build python-setuptools
 echo "%_unpackaged_files_terminate_build 0" > ~/.rpmmacros
-python setup.py bdist_rpm --requires="python-ply"
+python setup.py bdist_rpm  --release="$1" --requires="python-ply"
 mkdir dist_pkg
 cp dist/*.noarch.rpm dist_pkg
