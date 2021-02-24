b8a1138c7689b1b9a6f864aba32e9815fdd36e41
Miguel Caballer <micafer1@upv.es>
2021-02-24 16:15:35 +0100

    Update generate_deb.sh

--- packages/generate_deb.sh
+++ packages/generate_deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install -y python-stdeb
+apt install -y python-stdeb dh-python
 python setup.py --command-packages=stdeb.command sdist_dsc --depends "python-ply" bdist_deb
 mkdir dist_pkg
 cp deb_dist/*.deb dist_pkg
